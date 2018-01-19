021010fd217246a0887f57e76f490a526dc5b712
Massimo Cappellano <Massimo.Cappe@gmail.com>
2018-01-19 15:39:16 +0100

    improvements

--- README.md
+++ README.md
@@ -4,7 +4,57 @@
 
 [pthread_cond_timedwait manpage](http://man.yolinux.com/cgi-bin/man2html?cgi_command=pthread_cond_timedwait)
 
+## USE of `pthread_cond_timedwait`
+
 `cond_timewait.c` some proof with `pthread_cond_signal` and `pthread_cond_timedwait`.
+
+Thead functionCount increment counter and wait for 5 secs using `pthread_cond_timedwait`. 
+On `SIGINT` capture, the handler call `pthread_cond_signal` and the `pthread_cond_timedwait` in thead exit before timeout.
+
+```
+
+/**
+ * param timeInMs - timeout in msec. 
+ * 
+ * return -1 if SIGNAL arrived;
+ *         1 if timeout;
+ *         0 else
+ **/
+
+int wait(int timeInMs)
+{
+    struct timeval tv;
+    struct timespec ts;
+
+    gettimeofday(&tv, NULL);
+    ts.tv_sec = time(NULL) + timeInMs / 1000;
+    ts.tv_nsec = tv.tv_usec * 1000 + 1000 * 1000 * (timeInMs % 1000);
+    ts.tv_sec += ts.tv_nsec / (1000 * 1000 * 1000);
+    ts.tv_nsec %= (1000 * 1000 * 1000);
+
+    int n = pthread_cond_timedwait(&condition_var, &count_mutex, &ts);
+    if (n == 0) {
+        printf("SIGNALED!!!!\n");
+        return -1;
+    }
+        
+    else if (n == ETIMEDOUT) {
+        printf("TIMEOUT REACHED!!!!\n");
+        return 1;
+    } else {
+        return 0;
+    }
+        
+}
+
+```
+
+Question [pthread_cond_timedwait()](https://stackoverflow.com/questions/1486833/pthread-cond-timedwait).  
+Question [Calling pthread_cond_signal without locking mutex](https://stackoverflow.com/questions/4544234/calling-pthread-cond-signal-without-locking-mutex).
+
+
+
+
 
 
 
--- cond_timedwait.c
+++ cond_timedwait.c
@@ -4,19 +4,27 @@
 #include <sys/time.h>
 #include <unistd.h> 
 #include <pthread.h>
+#include  <signal.h>
 
 pthread_mutex_t count_mutex     = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  condition_var   = PTHREAD_COND_INITIALIZER;
 
-void *functionCount1();
-void *functionCount2();
+void *functionCount();
+
+int running = 1;
 int  count = 0;
 
-#define COUNT_DONE  10
-#define COUNT_HALT1  3
-#define COUNT_HALT2  6
+void  INThandler(int);
 
-void wait(int timeInMs)
+/**
+ * param timeInMs - timeout in msec. 
+ * 
+ * return -1 if SIGNAL arrived;
+ *         1 if timeout;
+ *         0 else
+ **/
+
+int wait(int timeInMs)
 {
     struct timeval tv;
     struct timespec ts;
@@ -30,82 +38,84 @@ void wait(int timeInMs)
     int n = pthread_cond_timedwait(&condition_var, &count_mutex, &ts);
     if (n == 0) {
         printf("SIGNALED!!!!\n");
+        return -1;
     }
         
     else if (n == ETIMEDOUT) {
         printf("TIMEOUT REACHED!!!!\n");
+        return 1;
+    } else {
+        return 0;
     }
         
 }
 
+
 void main()
 {
-   pthread_t thread1, thread2;
+   signal(SIGINT, INThandler);
+   
+   printf("PRESS Ctrl + C for stopping!!!\n");
 
-   pthread_create( &thread1, NULL, &functionCount1, NULL);
-   pthread_create( &thread2, NULL, &functionCount2, NULL);
+   pthread_t thread1;
+
+   pthread_create( &thread1, NULL, &functionCount, NULL);
 
    pthread_join( thread1, NULL);
-   pthread_join( thread2, NULL);
 
    printf("Final count: %d\n",count);
 
    exit(EXIT_SUCCESS);
 }
 
-// Write numbers 1-3 and 8-10 as permitted by functionCount2()
+void  INThandler(int sig)
+{
+     signal(sig, SIG_IGN);
+     
+     pthread_mutex_lock(&count_mutex);
 
-void *functionCount1()
+     // Condition of if statement has been met. 
+     // Signal to free waiting thread by freeing the mutex.
+     // Note: functionCount1() is now permitted to modify "count".
+     printf("--------------> SENDING SIGNAL!!!!\n");
+
+     pthread_cond_signal(&condition_var);
+
+     pthread_mutex_unlock(&count_mutex);
+
+}
+
+
+// increment counter and stop at signal 
+
+void *functionCount()
 {
       printf("STARTING functionCount1()\n");
-
-      // Lock mutex and then wait for signal to relase mutex
-      pthread_mutex_lock( &count_mutex );
-
-      printf("BEFORE WAIT!!!\n");
-      fflush(stdout);
-
-      wait(5000); // wait for 5 sec or for signal
       
-      printf("AFTER WAIT: Counter value functionCount1: %d\n",count);
+      while(running) {
 
-      pthread_mutex_unlock( &count_mutex );
+        // Lock mutex and then wait for signal to relase mutex
+        pthread_mutex_lock(&count_mutex);
+
+        printf("BEFORE WAIT!!!\n");
+
+        if( wait(5000)== -1 ){
+            printf("SETTED RUNNING FALSE\n");
+            running = 0;
+        }
+
+        count++;
+        
+        printf("AFTER WAIT: Counter value functionCount1: %d\n",count);
+
+        pthread_mutex_unlock(&count_mutex);
+
+      }
 
       return(NULL);
     
 }
 
-// Write numbers 4-7
 
-void *functionCount2()
-{
-    printf("STARTING functionCount2()\n");
-    for(;;)
-    {
-       pthread_mutex_lock( &count_mutex );
 
-       if( count > COUNT_HALT2 )
-       {
-          // Condition of if statement has been met. 
-          // Signal to free waiting thread by freeing the mutex.
-          // Note: functionCount1() is now permitted to modify "count".
-          printf("--------------> ********************** SENDING SIGNAL!!!!\n");
-          pthread_cond_signal( &condition_var );
 
-          pthread_mutex_unlock( &count_mutex );
-          break;
-       }
-       else
-       {
-          count++;
-          printf("Counter value functionCount2: %d\n",count);
-          // sleep(2);
-          wait(2 * 1000);
-       }
-
-       pthread_mutex_unlock( &count_mutex );
-
-       if(count >= COUNT_DONE) return(NULL);
-    }
-
-}
